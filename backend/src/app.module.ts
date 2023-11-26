108d4f6da08f710c9e6df60b1dfc548b9b71b90c
Anton Poroshkin <poroshkinanton18@gmail.com>
2023-11-26 23:38:12 +0300

    add notes module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    NotesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
